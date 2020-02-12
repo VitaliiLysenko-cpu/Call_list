b9a9a3d1e9f96fbe75f528f2560f46a88a1a4c3e
Vitalii <vit47903@gmail.com>
2020-02-12 10:03:49 +0200

    push preliminary  branch

--- app/src/main/java/com/example/calllist/activity/InfoOnSubscriber.java
+++ app/src/main/java/com/example/calllist/activity/InfoOnSubscriber.java
@@ -1,10 +1,8 @@
 package com.example.calllist.activity;
 
 import android.os.Bundle;
-
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.calllist.R;
 
 public class InfoOnSubscriber extends AppCompatActivity {
